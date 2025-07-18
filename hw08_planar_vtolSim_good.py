478f125dd6c1ad598cdee89844de98de3c7a20ec
Ethan Brown <88006720+swiftwarkite2@users.noreply.github.com>
2025-07-18 14:31:04 -0700

    Update hw08_planar_vtolSim_good.py

--- hw08_planar_vtolSim_good.py
+++ hw08_planar_vtolSim_good.py
@@ -11,9 +11,10 @@ from ctrlPD_IO import ctrlPD
 # instantiate satellite, controller, and reference classes
 vtol = Dynamics(alpha=0.0)
 controller = ctrlPD()
+# Zreference controls the characteristics of the horizontal reference signal
 zreference = signalGenerator(amplitude=5, frequency=0.11, y_offset = 0)
 torque = signalGenerator(amplitude=5, frequency=0.02)
-
+# Href controls the characteristics of the vertical reference signal
 force = signalGenerator(amplitude=5, frequency=0.02)
 hreference = signalGenerator(amplitude=5, frequency=0.09, y_offset = 0)
 noise = signalGenerator(amplitude=0.01)
@@ -32,7 +33,15 @@ while t < P.t_end:  # main simulation loop
     while t < t_next_plot:  
         d = 0
         z_ref = zreference.sin(t)
+      # z_ref takes the zreference signal characteristics (amplitude, frequency, y_offset) from above 
+      # and creates a sin wave signal with them shown as the green line in the z(m) plot in figure 1. 
+      # the Y axis on z(m) plot is equal to the X axis on figure 2
+
         h_ref = hreference.square(t)
+      # h_ref takes the hreference signal characteristics (amplitude, frequency, y_offset) from above 
+      # and creates a square wave signal with them shown as the green line in the h(m) plot in figure 
+      # 1. the Y axis on h(m) plot is equal to the Y axis on figure 2
+
         r = np.array([[z_ref],[h_ref]])
         x = vtol.state
         u = controller.update(r,x)
@@ -41,7 +50,7 @@ while t < P.t_end:  # main simulation loop
         tor = (fr-fl)*P.d
         y = vtol.update([[fr+d],[fl+d]])  # Propagate the dynamics
         t = t + P.Ts  # advance time by Ts
-        f = (fr+fl)
+        f = (fr+fl) # fr is force of right rotor, fl is for of left rotor
     # update animation and data plots
     animation.update(vtol.state)
     dataPlot.update(t, vtol.state, z_ref, h_ref, f, tor)

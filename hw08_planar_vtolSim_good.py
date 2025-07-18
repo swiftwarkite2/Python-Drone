fee8321f25d8f4027f8062e44b67f1fdefb541d7
Ethan Brown <88006720+swiftwarkite2@users.noreply.github.com>
2025-07-18 14:27:26 -0700

    Update hw08_planar_vtolSim_good.py

--- hw08_planar_vtolSim_good.py
+++ hw08_planar_vtolSim_good.py
@@ -1,3 +1,4 @@
+# I created this file from the following github https://github.com/byu-controlbook/controlbook_public
 import matplotlib.pyplot as plt
 import VTOLParam as P
 from signalGenerator import signalGenerator
@@ -29,47 +30,21 @@ while t < P.t_end:  # main simulation loop
     t_next_plot = t + P.t_plot
     # updates control and dynamics at faster simulation rate
     while t < t_next_plot:  
-        
-        #d = disturbance.step(t)
         d = 0
         z_ref = zreference.sin(t)
         h_ref = hreference.square(t)
-        #r = reference.square(t)
         r = np.array([[z_ref],[h_ref]])
         x = vtol.state
-        #f = force.square(t)
-        #tor = torque.square(t)
-        #u = controller.update(r,x) #
-        #forcet = controller.update(f,x)
-        #torquet = controller.update(tor,x)
-        #fr = (forcet/2)+(torquet/(2*P.d))
-        #fr = (f/2)+(tor/(2*P.d)) #
-        #fl = (forcet/2)-(torquet/(2*P.d))
-        #fl = (f/2)-(tor/(2*P.d)) #
-        #u = np.array([fl, fr]) #
         u = controller.update(r,x)
         fr = u[0][0]
         fl = u[1][0]       
-       
-        #fr_t = u[0][0]
-        #fl_t = u[1][0]
-        #tor = u[2][0]
         tor = (fr-fl)*P.d
-        #fl = fl_t - tor/(2*P.d)
-        #fr = fr_t + tor/(2*P.d)
-        #fl = fl_t - tor/4
-        #fr = fr_t + tor/4
-
         y = vtol.update([[fr+d],[fl+d]])  # Propagate the dynamics
         t = t + P.Ts  # advance time by Ts
         f = (fr+fl)
-        
     # update animation and data plots
-    
     animation.update(vtol.state)
-    #dataPlot.update(t, vtol.state, z_ref, h_ref, forcet, torquet)
     dataPlot.update(t, vtol.state, z_ref, h_ref, f, tor)
-    #dataPlot.update(t, VTOL.state, z_ref, h_ref, f, tor) #
     plt.pause(.3)  
 
 # Keeps the program from closing until the user presses a button.

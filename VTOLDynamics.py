7d3c96516b13a6a980494c2dccdc44606cfab024
Ethan Brown <88006720+swiftwarkite2@users.noreply.github.com>
2025-07-18 14:37:33 -0700

    Update VTOLDynamics.py

--- VTOLDynamics.py
+++ VTOLDynamics.py
@@ -35,8 +35,6 @@ class Dynamics:
     def update(self, u):
         # This is the external method that takes the input u at time
         # t and returns the output y at time t.
-        # saturate the input torque
-        # u = saturate(u, self.torque_limit)
         self.rk4_step(u)  # propagate the state by one time sample
         y = self.h()  # return the corresponding output
         return y
@@ -51,12 +49,12 @@ class Dynamics:
         thetadot = state[5][0]
         fl = u[1][0]
         fr = u[0][0]
-        
-        #fl = tau
-        #fr = tau
-        # fl = state[6][0]
-        # fr = state[7][0]
+
         # The equations of motion.
+        # the below code is used to implement the equations of motion for this vtol system. 
+        # M is a matrix representing what is multiplied by zddot, hddot, and thetaddot while C is everything on the other side of the equal sign. 
+        # Zddot is the second derivative of the horizontal dimension which is equivalent to the acceleration in that dimension. 
+        # Hddot is the same except its the vertical dimension. Thetaddot is the same but for turn angel
         M = np.array([[self.mc + 2* self.mr, 0, 0],
                       [0, self.mc + 2*self.mr, 0],
                       [0, 0, self.Jc + 2 * self.mr * self.d**2]])
@@ -72,12 +70,6 @@ class Dynamics:
         xdot = np.array([ [zdot],[hdot],[thetadot], [zddot],[hddot], [thetaddot]])
         return xdot
 
-    #def h(self):
-        # return y = h(x)
-    #    theta = self.state[0][0]
-    #    phi = self.state[1][0]
-    #    y = np.array([[theta], [phi]])
-    #    return y
     def h(self):
         # return y = h(x)
         z = self.state[0][0]
@@ -93,9 +85,3 @@ class Dynamics:
         F3 = self.f(self.state + self.Ts / 2 * F2, u)
         F4 = self.f(self.state + self.Ts * F3, u)
         self.state += self.Ts / 6 * (F1 + 2 * F2 + 2 * F3 + F4)
-
-        
-##def saturate(u, limit):
-  ##  if abs(u) > limit:
-    ##    u = limit*np.sign(u)
-    ##return u
